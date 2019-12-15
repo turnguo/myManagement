593ab042fd97fb59ac78f94509f95d1c229b1a9a
turnguo <451202697@qq.com>
2019-12-15 16:29:30 +0800

    支持https

--- src/main/java/net/sppan/base/Application.java
+++ src/main/java/net/sppan/base/Application.java
@@ -1,14 +1,26 @@
 package net.sppan.base;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
 public class Application{
+	@Value("${server.port}")
+	private int serverPort;
+
+	@Value("${server.http.port}")
+	private int serverHttpPort;
 
 	private static Logger logger = LoggerFactory.getLogger(Application.class);
 	/**
@@ -17,5 +29,35 @@ public class Application{
 		SpringApplication.run(Application.class, args);
 		logger.debug("启动成功");
 	}
-	
+	/**
+	 * http重定向到https
+	 * @return
+	 */
+	@Bean
+	public TomcatEmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+		return tomcat;
+	}
+	private Connector initiateHttpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		//需要重定向的http端口
+		connector.setPort(serverHttpPort);
+		connector.setSecure(false);
+		//设置重定向到https端口
+		connector.setRedirectPort(serverPort);
+		return connector;
+	}
 }
